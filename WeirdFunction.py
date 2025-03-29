dc0c22a33e284cbceceb9a017611b63685c9d947
Maria Helena <helena.varelakg@gmail.com>
2025-03-29 00:14:38 -0300

    WeirdFunction

---
+++ WeirdFunction.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+f = (t**2)+(2 * t) + 3
+
+primeiro = f + t
+segundo = (t**2)+(2 * t) + 3
+terceiro = (segundo**2)+(2 * segundo) + 3
+quarto = (primeiro**2)+(2 * primeiro) + 3
+quinto = terceiro + quarto
+sexto = (quinto**2)+(2 * quinto) + 3
+print(sexto)
